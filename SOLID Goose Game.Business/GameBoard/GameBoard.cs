97bd9c591872969e8182c1a295b6f96bb4c3636d
Gilles Fabry <gilles.fabry@hotmail.be>
2024-05-06 09:23:52 +0200

    Added BoardGame class and implemented interface

---
+++ SOLID Goose Game.Business/GameBoard/GameBoard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Goose_Game.Business.GameBoard
+{
+    internal class GameBoard : IGameBoard
+    {
+        public int[] Boardsize { get; private set; } = new int[63];
+    }
+}
