1bf7ee285c36660867cb2d485e8e3c10655205f8
Gilles Fabry <gilles.fabry@hotmail.be>
2024-05-06 15:54:02 +0200

    Tested and successfully passed BridgeCase test

--- SOLID Goose Game.Tests/Cases.Tests/BridgeCase.Tests.cs
+++ SOLID Goose Game.Tests/Cases.Tests/BridgeCase.Tests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SOLID_Goose_Game.Business.Factories;
+using SOLID_Goose_Game.Business.GameBoard;
+using SOLID_Goose_Game.Business.GameState;
+using SOLID_Goose_Game.Business.Players;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,25 @@ namespace SOLID_Goose_Game.Tests.Cases.Tests
 {
     internal class BridgeCase
     {
+        [Test]
+        public void CheckIfDeathCaseResetsPlayerTo0()
+        {
+            //Arrange
+            ICaseFactory caseFactory = new CaseFactory();
+            IGameState gameState = new GameState();
+            Player player = new Player();
+            player.CurrentPosition = 6;
+            player.StartingPosition = 0;
+            IGameBoard gameBoard = new GameBoard(caseFactory, gameState);
+
+            int expectedPosition = 12;
+
+            //Act
+            gameBoard.FillInBoardCases();
+            gameBoard.HandleCaseType(player);
+
+            //Assert
+            Assert.AreEqual(expectedPosition, player.CurrentPosition);
+        }
     }
 }
