d125e441a076e15b126442636d7965302f18faa4
Gilles Fabry <gilles.fabry@hotmail.be>
2024-05-07 15:03:03 +0200

    Set up test file for EndCase

---
+++ SOLID Goose Game.Tests/Cases.Tests/EndCase.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Goose_Game.Tests.Cases.Tests
+{
+    internal class EndCaseTests
+    {
+        [Test]
+        public void AssertsIfPlayerDidNotMoveOntoEndCaseExactly()
+        {
+
+        }
+        [Test]
+        public void AssertsIfPlayerDicerollSumExceedsIDAndSendsPlayerBackEqualToDifference()
+        {
+
+        }
+    }
+}
