7ad81fcd022af1016dcaa04bdfe5573c859ba35a
Gilles Fabry <gilles.fabry@hotmail.be>
2024-05-07 10:57:18 +0200

    Added first draft of WellCase Tests

---
+++ SOLID Goose Game.Tests/Cases.Tests/WellCase.Tests.cs
@@ -0,0 +1,37 @@
+﻿using SOLID_Goose_Game.Business.Factories;
+using SOLID_Goose_Game.Business.GameBoard;
+using SOLID_Goose_Game.Business.GameState;
+using SOLID_Goose_Game.Business.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Goose_Game.Tests.Cases.Tests
+{
+    internal class WellCase
+    {
+        [Test]
+        public void AssertsThatTheWellCorrectlyTakesInAPlayerIndefinitely()
+        {
+            //Arrange
+            ICaseFactory caseFactory = new CaseFactory();
+            IGameState gameState = new GameState();
+            Player player = new Player("Speler");
+            int[] dieRolls = new int[2];
+            dieRolls = [4, 2];
+            player.CurrentPosition = 25;
+            player.StartingPosition = 25;
+            IGameBoard gameBoard = new GameBoard(caseFactory, gameState);
+
+            int expectedPosition = 31;
+
+            //Act
+            gameBoard.FillInBoardCases();
+            player.DetermineNewPosition(dieRolls);
+
+            //Assert
+        }
+    }
+}
