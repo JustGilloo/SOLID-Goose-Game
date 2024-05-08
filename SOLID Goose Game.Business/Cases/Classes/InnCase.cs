3a73e9df4249ea73eebf181c74fab800b2733d29
Gilles Fabry <gilles.fabry@hotmail.be>
2024-05-08 15:23:32 +0200

    Added log landing message to InnCase

--- SOLID Goose Game.Business/Cases/Classes/InnCase.cs
+++ SOLID Goose Game.Business/Cases/Classes/InnCase.cs
@@ -21,7 +21,7 @@ namespace SOLID_Goose_Game.Business.Cases.Classes
         public void ResolveCase(Player player)
         {
             ApplyInnCaseEffect(player);
-            logger.LogMessage(this.Type.ToString());
+            logger.LogMessage($"{player.PlayerName} landde op een herberg en blijft er {player.EffectDurationInTurns} beurten slapen.");
         }
         public void ApplyInnCaseEffect(Player player)
         {
